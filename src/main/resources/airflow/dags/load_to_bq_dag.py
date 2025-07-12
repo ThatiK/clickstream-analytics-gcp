e7c610bff8d326cd2a3881f631ca8308d8049dcb
Sai Karthik Thati <karthik.dataeng@gmail.com>
2025-07-12 16:52:04 +0530

    CAEC-14: Debug

--- src/main/resources/airflow/dags/load_to_bq_dag.py
+++ src/main/resources/airflow/dags/load_to_bq_dag.py
@@ -1,31 +1,33 @@
 import os
 from datetime import datetime, timedelta
-
 from airflow import DAG
 from airflow.models import Variable
-from airflow.providers.google.cloud.transfers.gcs_to_bigquery import GCSToBigQueryOperator
+from airflow.providers.google.cloud.operators.dataproc import DataprocCreateBatchOperator
 from airflow.operators.dummy import DummyOperator
 
-# Airflow Variables
-PROJECT_ID      = os.getenv("CAEC_PROJECT_ID")
-REGION          = os.getenv("CAEC_REGION")
-SCRIPTS_BUCKET  = os.getenv("CAEC_SCRIPTS_BUCKET")         
-BQ_DATASET      = os.getenv("CAEC_BQ_DATASET", "caec_analytics")
-ENV             = os.getenv("CAEC_ENV")                    
+# Airflow Variables 
+PROJECT_ID   = os.getenv("CAEC_PROJECT_ID")
+REGION       = os.getenv("CAEC_REGION")
+SCRIPTS_BKT  = os.getenv("CAEC_SCRIPTS_BUCKET")   
+ENV          = os.getenv("CAEC_ENV")              
 
+# GCS URIs 
+SPARK_MAIN   = f"gs://{SCRIPTS_BKT}/spark/load_to_bq_clickstream.py"
+PROPS_URI    = f"gs://{SCRIPTS_BKT}/etc/{ENV}.properties"
+CONNECTOR = "gs://spark-lib/bigquery/spark-bigquery-with-dependencies_2.12-0.29.0.jar"
 
-GCS_SOURCE_URI  = "gs://{SCRIPTS_BUCKET}/sessionized/events/event_date_part={{ ds_nodash }}/*.parquet"
-BQ_TABLE        = f"{PROJECT_ID}.{BQ_DATASET}.clickstream_sessions"
 
 def generate_timestamped_batch_name(base_name):
     ts = datetime.now().strftime("%Y%m%d-%H%M%S")
     return f"{base_name}-{ts}"
 
-BASE_BATCH_NAME = "caec-load-to-bq-clickstream"
+BASE_BATCH_NAME = "caec-lot-to-bq-clickstream"
 BATCH_NAME = generate_timestamped_batch_name(BASE_BATCH_NAME)
+
 
 default_args = {
     "owner": "caec",
+    "depends_on_past": False,
     "email_on_failure": False,
     "email_on_retry": False,
     #"retries": 1,
@@ -33,28 +35,41 @@ default_args = {
 }
 
 with DAG(
-    dag_id="caec_load_clickstream_to_bq_dag",
-    start_date=datetime(2025, 7, 1),
-    schedule_interval=None,            # typically triggered by upstream DAG
-    catchup=False,
+    dag_id="caec_load_to_bq_clickstream_dag",
     default_args=default_args,
-    tags=["caec", "clickstream", "bq"],
+    start_date=datetime(2025, 7, 1),
+    schedule_interval=None,          
+    catchup=False,
+    tags=["caec", "clickstream"],
 ) as dag:
 
     start = DummyOperator(task_id="start")
 
-    # Load from GCS into BigQuery
-    load_to_bq = GCSToBigQueryOperator(
-        task_id="gcs_to_bq",
-        bucket=SCRIPTS_BUCKET,
-        source_objects=["sessionized/events/event_date_part={{ ds_nodash }}/*.parquet"],
-        destination_project_dataset_table=BQ_TABLE,
-        source_format="PARQUET",
-        write_disposition="WRITE_TRUNCATE",          
-        time_partitioning={"type": "DAY"},
-        gcp_conn_id="google_cloud_default",          
+    sessionize_batch = DataprocCreateBatchOperator(
+        task_id="load_to_bq_batch",
+        batch={
+            "pyspark_batch": {
+                "main_python_file_uri": SPARK_MAIN,
+                "python_file_uris": [PROPS_URI],           
+                "args": [
+                    "--conf", f"{ENV}",                    
+                ],
+                "jar_file_uris": [CONNECTOR],
+            },
+            "runtime_config": {
+                "version": "2.1"  
+            },
+            "environment_config": {
+                "execution_config": {
+                    "service_account": f"caec-data-eng-sa@{PROJECT_ID}.iam.gserviceaccount.com"
+                }
+            },
+        },
+        batch_id=BATCH_NAME,        # unique per run (includes H M S)
+        region=REGION,
+        project_id=PROJECT_ID,
     )
 
     end = DummyOperator(task_id="end")
 
-    start >> load_to_bq >> end
+    start >> sessionize_batch >> end
